ac6cafb0859a037288d0fc0ed151df3bb290ef5f
bt1 <bt1@Benjamins-MacBook-Pro.local>
2016-11-20 12:32:10 -0500

    added homework assignments

---
+++ InstaChat/ICSignupViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ICSignupViewController.swift
+//  InstaChat
+//
+//  Created by Benjamin Tan on 11/20/16.
+//  Copyright © 2016 TheAustinSpace. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ICSignupViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var emailTextfield: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // Task 3: Ensure that the username only allows alphanumeric characters
+        // return true if its a alphanumeric, return false if not
+        
+        return true
+    }
+    
+    @IBAction func signupPressed(_ sender: Any) {
+        // Task 1: Verify that email is an actual email. Things to check are: .com, .net, .co, includes @ and check the textfield is blank
+        
+        // Task 2: Check for password: requirements: min length of 6 characters
+        
+        
+        // Task 4: Submit to firebase
+        /*
+         FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+         // ...
+         
+         //Task 5: Store the "user id" as Defaults[.user_id]
+         https://github.com/radex/SwiftyUserDefaults
+         }
+         */
+        
+    
+
+    }
+    
+    
+    
+    
+}
