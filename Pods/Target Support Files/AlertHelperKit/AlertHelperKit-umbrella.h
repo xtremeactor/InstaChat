ee7cd7f988ab652d46570179b93639df975d11a2
Jason Buchel <xtremeactor@Jasons-MacBook-Air.local>
2016-11-20 11:44:32 -0500

    created login UI

---
+++ Pods/Target Support Files/AlertHelperKit/AlertHelperKit-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "AlertHelperKit.h"
+
+FOUNDATION_EXPORT double AlertHelperKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char AlertHelperKitVersionString[];
+
