ee702cfec0451469cd8df00b59fb5741ea76ff1d
bt1 <bt1@Benjamins-MacBook-Pro.local>
2016-11-27 10:50:31 -0500

    rm dup

--- InstaChat/AppDelegate.swift
+++ InstaChat/AppDelegate.swift
@@ -17,8 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FIRApp.configure()
-        // Override point for customization after application launch.
-        FIRApp.configure()
         return true
     }
 
